caad978527a57159821ba41b5a50cfb7eb0d6b3b
DiorAbjalilov <abjalilovdiyor@gmail.com>
2022-01-20 17:47:42 +0500

    add mongoose data connect

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
 const { engine } = require("express-handlebars");
 const dotenv = require("dotenv");
 const homeRoutes = require("./routes/homeRoutes");
@@ -24,6 +25,19 @@ app.set("views", "./views");
 // Initialize routes
 app.use("/", homeRoutes);
 app.use("/posters", postersRoutes);
+
+// mongoose
+const connectDB = async () => {
+  const conn = await mongoose.connect("mongodb://localhost:27017/postersApp", {
+    useNewUrlParser: true,
+    // useCreateIndex: true,
+    // useFindAndModify: false,
+    useUnifiedTopology: true,
+  });
+  console.log(`MongoDB Connected:${conn.connection.host}`);
+};
+
+connectDB();
 
 const PORT = process.env.PORT || 3000;
 
