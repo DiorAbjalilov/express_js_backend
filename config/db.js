2c04267085cfbf276628a393b59f4f7ecfd892d3
DiorAbjalilov <abjalilovdiyor@gmail.com>
2022-01-28 17:56:39 +0500

    connect mongodb online database

--- config/db.js
+++ config/db.js
@@ -1,26 +1,26 @@
 const mongoose = require("mongoose");
 
-// const connectDB = async () => {
-//   const conn = await mongoose.connect(process.env.MONGO_URI, {
-//     useNewUrlParser: true,
-//     // useCreateIndex: true,
-//     // useFindAndModify: false,
-//     useUnifiedTopology: true,
-//   });
-//   console.log(`MongoDB Connected:${conn.connection.host}`);
-// };
-
 const connectDB = async () => {
-  try {
-    mongoose.set("useNewUrlParser", true);
-    mongoose.set("useUnifiedTopology", true);
-    mongoose.set("useCreateIndex", true);
-    mongoose.set("useFindAndModify", false);
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB Connected ${conn.connection.host}");
-    return;
-  } catch (err) {
-    process.exit(1);
-  }
+  const conn = await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    // useCreateIndex: true,
+    // useFindAndModify: false,
+    useUnifiedTopology: true,
+  });
+  console.log(`MongoDB Connected:${conn.connection.host}`);
 };
+
+// const connectDB = async () => {
+//   try {
+//     mongoose.set("useNewUrlParser", true);
+//     mongoose.set("useUnifiedTopology", true);
+//     mongoose.set("useCreateIndex", true);
+//     mongoose.set("useFindAndModify", false);
+//     const conn = await mongoose.connect(process.env.MONGO_URI);
+//     console.log("MongoDB Connected ${conn.connection.host}");
+//     return;
+//   } catch (err) {
+//     process.exit(1);
+//   }
+// };
 module.exports = connectDB;
